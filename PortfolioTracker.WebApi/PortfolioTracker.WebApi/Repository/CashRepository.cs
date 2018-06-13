8efffb2120d0b2798f38e9ebb0e62774b85884ec
Ganesh Jangal <ganesh.jangal@outlook.com>
2018-06-12 22:26:34 -0400

    Corrected query to retuen cash amount for a give portfolio correctly

--- PortfolioTracker.WebApi/PortfolioTracker.WebApi/Repository/CashRepository.cs
+++ PortfolioTracker.WebApi/PortfolioTracker.WebApi/Repository/CashRepository.cs
@@ -64,7 +64,7 @@ namespace PortfolioTracker.WebApi.Repository
             using (var connection = new SqlConnection(configuration.GetValue<string>("ConnectionStrings:MarketData")))
             {
                 connection.Open();
-                var sql = $"select PortfolioId, SUM(Amount) from dbo.Cash where PortfolioId={portfolioId} and Date <= '{asOf}' group by PortfolioId";
+                var sql = $"select PortfolioId, SUM(Amount) as Amount from dbo.Cash where PortfolioId={portfolioId} and Date <= '{asOf}' group by PortfolioId";
                 return (await connection.QueryAsync<Cash>(sql)).FirstOrDefault();
             }
         }
